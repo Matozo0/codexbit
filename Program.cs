d59e95664b721547623002861ce3daaa75f5a8d7
Matozo <filipaomat100@gmail.com>
2025-02-19 22:25:14 -0300

    feat(program.cs): configurado o program.cs para funcionar com sqlite e tambem o appsettings para o datasource

--- Program.cs
+++ Program.cs
@@ -1,20 +1,16 @@
+using CodexBit.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BlogContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 app.UseRouting();
-
-app.UseAuthorization();
-
 app.MapStaticAssets();
 
 app.MapControllerRoute(
