52d9711fe1fe9cce8990015a1b51a933ecd1feaf
Matozo <filipaomat100@gmail.com>
2025-02-19 22:04:56 -0300

    feat(blogcontext): representação do banco de dados

--- Models/CommentModel.cs
+++ Models/CommentModel.cs
@@ -5,23 +5,29 @@ namespace CodexBit.Models;
 
 public class CommentModel
 {
+    // Identificação
     [Key]
     public int Id { get; set; }
 
+    // Usuário que comentou
     [Required]
     [MaxLength(200)]
     public string Username { get; set; }
-    
+        
+    // Conteúdo do comentário    
     [Required]
     [MaxLength(200)]
     public string Content { get; set; }
 
+    // Data de criação do comentário
     [DataType(DataType.DateTime)]
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
+    // Identificação do post pertecente ao comentário
     [Required]
     public int PostId { get; set; }
 
+    // Objeto Post que obtém o post
     [Required]
     public PostModel Post { get; set; }
 }
--- Models/PostModel.cs
+++ Models/PostModel.cs
@@ -5,28 +5,36 @@ namespace CodexBit.Models;
 
 public class PostModel
 {
+    // Identificação do post
     [Key]
     public int Id { get; set; }
 
+    // Titulo do post
     [Required]
     [MaxLength(200)]
     public string Title { get; set; }
 
+    // Breve descrição do post
     [Required]
     [MaxLength(500)]
     public string Description { get; set; }
     
+    // Thumbnail do post
     [Required]
     public string ImageURL { get; set; }
 
+    // Texto do post em formato Markdown
     [Required]
     public string ContentMarkdown { get; set; }
 
+    // Data de criação do post
     [DataType(DataType.DateTime)]
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
+    // Data de atualização do post
     [DataType(DataType.DateTime)]
     public DateTime UpdatedAt { get; set; }     
 
+    // Comentários do post
     public List<CommentModel> Comments { get; set; } = new();
 }
